58f8fa85e2ff5834b997a77279720dee2f898a95
Mhd <mohammedalmahmood96@gmail.com>
2020-08-22 02:09:01 +0300

    11

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,34 +12,31 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 
-*/
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+ */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-
+Request-With');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
 });
 
-
-
 //api/v1/Auth/user/>>
-Route::prefix('user')->group( function(){
+Route::prefix('user')->group(function () {
     //regiser
     Route::post('register', 'api\v1\auth\RegisterController@register');
     //login
-    Route::post('login','api\v1\auth\LoginController@login');
+    Route::post('login', 'api\v1\auth\LoginController@login');
     //testing
-    Route::middleware('auth:api')->get('/all' ,'api\v1\auth\LoginController@getusers');
+    Route::middleware('auth:api')->get('/all', 'api\v1\auth\LoginController@getusers');
 });
-
-
 
 Route::middleware('auth:api')->prefix('info')->group(function () {
 
     Route::get('/create', 'api\v1\InfoController@create')->name('info.create');
-   Route::post('/store', 'api\v1\InfoController@store')->name('info.store');
+    Route::post('/store', 'api\v1\InfoController@store')->name('info.store');
     //edit profile
     Route::get('{id}/edit', 'api\v1\InfoController@edit')->name('info.edit');
     Route::patch('{id}/update', 'api\v1\InfoController@update')->name('info.update');
@@ -52,14 +48,11 @@ Route::middleware('auth:api')->prefix('info')->group(function () {
     //rate
     Route::post('notifications', 'api\v1\UserNotificationController@rate')->name('rateTeacher');
 
-
     //complete image info after selection sybjects
     Route::get('complete', 'api\v1\infoController@uploadImages')->name('InfoComplete');
     Route::post('complete', 'api\v1\infoController@storeImages')->name('InfoCompleted');
 
 });
-
-
 
 Route::middleware('auth:api')->prefix('subject')->group(function () {
 //select subject to teach
@@ -68,16 +61,12 @@ Route::middleware('auth:api')->prefix('subject')->group(function () {
     Route::post('select', 'api\v1\SubjectController@store')->name('sub.store');
 });
 
-
-
 Route::middleware('auth:api')->prefix('teacher')->group(function () {
 //request  a teacher for   a subject
     Route::get('request', 'api\v1\TeacherController@create')->name('sub.request');
     Route::post('request', 'api\v1\TeacherController@index')->name('sub.index');
 
 });
-
-
 
 Route::middleware('auth:api')->prefix('data')->group(function () {
 //request  a teacher for   a subject
@@ -87,12 +76,9 @@ Route::middleware('auth:api')->prefix('data')->group(function () {
 
 });
 
-
 //to choose materials to teach
 Route::middleware('auth:api')->prefix('schools')->group(function () {
     Route::get('material/select', 'api\v1\SchoolController@select');
     Route::post('material/select', 'api\v1\SchoolController@save');
     Route::get('schools', 'api\v1\SchoolController@index');
 });
-
-
